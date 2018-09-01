26040ebe64d091240d27dcd45c0fb0712d05562f
chuck-durst <chuckdurst67@gmail.com>
2018-09-01 22:19:17 +0200

    First build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: "index.js"
+  },
+  devtool: false,
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  }
+
+};
